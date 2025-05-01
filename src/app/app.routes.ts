74eb87b0e9089b34aa0ce1b7a62481c921f406d9
ayman-19 <152824182+ayman-19@users.noreply.github.com>
2025-05-01 03:17:12 +0300

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { EmployeesComponent } from './Pages/employees/employees.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{path:'',redirectTo:'employees', pathMatch:'full'}, {path:'employees', title:'employees', component:EmployeesComponent}];
